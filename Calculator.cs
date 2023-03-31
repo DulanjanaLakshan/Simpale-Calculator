62e6f0a77bf8866a3eb316b184c3dbdf154b8b6e
dulanjanalakshan <dulanjana20013@gmail.com>
2023-03-31 07:25:04 +0530

    create simple calculator

---
+++ Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Calculator
+    {
+        private double num1;
+        private double num2;
+        public Calculator(double num1,double num2) 
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public double Add()
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract()
+        {
+            return num1 - num2;
+        }
+
+        public double Multiply()
+        {
+            return num1 * num2;
+        }
+
+        public double Divide()
+        {
+            if(num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot Divide By Zero");
+            }
+            return num2 / num1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using calculator;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        double num1, num2;
+        Console.Write("Enter First Number: ");
+        num1 = double.Parse(Console.ReadLine());
+        Console.Write("Enter Second Number: ");
+        num2 = double.Parse(Console.ReadLine());
+
+        Calculator cal=new Calculator(num1, num2);
+        Console.WriteLine($"{num1} + {num2} = {cal.Add()}");
+        Console.WriteLine($"{num1} - {num2} = {cal.Subtract()}");
+        Console.WriteLine($"{num1} * {num2} = {cal.Multiply()}");
+        Console.WriteLine($"{num1} / {num2} = {cal.Divide()}");
+    }
+}
